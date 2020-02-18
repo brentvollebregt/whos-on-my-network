03edb60f9488c5379b5d830f6c27496152db69ba
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-02-18 18:06:16 +1300

    Use DateRangeSelector in /scans

--- webapp/src/pages/Scans/Scans.tsx
+++ webapp/src/pages/Scans/Scans.tsx
@@ -4,148 +4,61 @@ import { useTitle, navigate } from "hookrouter";
 import PageSizeWrapper from "../../components/PageSizeWrapper";
 import { getScansByFilter } from "../../api";
 import { ScanSummary } from "../../api/dto";
-import {
-  Table,
-  Spinner,
-  InputGroup,
-  FormControl,
-  DropdownButton,
-  Dropdown
-} from "react-bootstrap";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { DateTime, Interval } from "luxon";
+import { Table, Spinner } from "react-bootstrap";
+import { DateTime } from "luxon";
 import { useLocalStorage } from "@rehooks/local-storage";
 import { genericApiErrorMessage } from "../../utils/toasts";
+import DateRangeSelector from "../../components/DateRangeSelector";
 
-const dateRanges = ["1 Day", "1 Week", "1 Month"];
-const dateRangeToInterval: { [key: string]: Interval } = {
-  "1 Day": Interval.fromDateTimes(
-    DateTime.local()
-      .minus({ days: 1 })
-      .startOf("day"),
-    DateTime.local().endOf("day")
-  ),
-  "1 Week": Interval.fromDateTimes(
-    DateTime.local()
-      .minus({ weeks: 1 })
-      .startOf("day"),
-    DateTime.local().endOf("day")
-  ),
-  "1 Month": Interval.fromDateTimes(
-    DateTime.local()
-      .minus({ months: 1 })
-      .startOf("day"),
-    DateTime.local().endOf("day")
-  )
-};
-const defaultStartDateRange = "1 Day";
-const defaultStartDate = dateRangeToInterval[defaultStartDateRange].start;
-const defaultEndDate = dateRangeToInterval[defaultStartDateRange].end;
+const defaultStartDate = DateTime.local()
+  .minus({ weeks: 1 })
+  .startOf("day");
+const defaultEndDate = DateTime.local().endOf("day");
 
 const Scans: React.FunctionComponent = () => {
   useTitle(`Scans - ${Constants.title}`);
 
   const [scans, setScans] = useState<ScanSummary[] | undefined>(undefined);
-  const [startAndEndDates, setStartAndEndDates] = useLocalStorage<
+  const [storedStartAndEndDates, setStoredStartAndEndDates] = useLocalStorage<
     [string, string]
-  >("scans_selectedDates", [defaultStartDate.toISO(), defaultEndDate.toISO()]);
+  >("home_selectedDates", [defaultStartDate.toISO(), defaultEndDate.toISO()]);
 
   const getStartDate = () =>
-    startAndEndDates === null
+    storedStartAndEndDates === null
       ? defaultStartDate
-      : DateTime.fromISO(startAndEndDates[0]);
+      : DateTime.fromISO(storedStartAndEndDates[0]);
   const getEndDate = () =>
-    startAndEndDates === null
+    storedStartAndEndDates === null
       ? defaultEndDate
-      : DateTime.fromISO(startAndEndDates[1]);
+      : DateTime.fromISO(storedStartAndEndDates[1]);
 
   useEffect(() => {
     getScansByFilter(undefined, getStartDate(), getEndDate())
       .then(s => setScans(s))
       .catch(err => genericApiErrorMessage("scans"));
-  }, [startAndEndDates]);
+  }, [storedStartAndEndDates]);
+
+  const getStartAndEndDates = (): [DateTime, DateTime] => [
+    getStartDate(),
+    getEndDate()
+  ];
+
+  const setStartAndEndDates = (startDate: DateTime, endDate: DateTime) => {
+    setStoredStartAndEndDates([startDate.toISO(), endDate.toISO()]);
+  };
 
   const onScanClick = (scanId: number) => () => navigate(`/scans/${scanId}`);
-  const onStartDateSelection = (date: Date | null) =>
-    date !== null &&
-    setStartAndEndDates([
-      DateTime.fromJSDate(date).toISO(),
-      getEndDate().toISO()
-    ]);
-  const onEndDateSelection = (date: Date | null) =>
-    date !== null &&
-    setStartAndEndDates([
-      getStartDate().toISO(),
-      DateTime.fromJSDate(date).toISO()
-    ]);
-  const onDateRangeSelection = (range: string) => () =>
-    setStartAndEndDates([
-      dateRangeToInterval[range].start.toISO(),
-      dateRangeToInterval[range].end.toISO()
-    ]);
 
   return (
     <PageSizeWrapper>
       <h1 className="text-center">Scans</h1>
 
-      <InputGroup className="mb-3">
-        <InputGroup.Text
-          style={{
-            borderTopRightRadius: 0,
-            borderBottomRightRadius: 0,
-            borderRight: "none"
-          }}
-        >
-          From
-        </InputGroup.Text>
-        <DatePicker
-          selected={getStartDate().toJSDate()}
-          onChange={onStartDateSelection}
-          customInput={
-            <FormControl
-              style={{
-                borderRadius: 0
-              }}
-            />
-          }
-          maxDate={getEndDate().toJSDate()}
-          dateFormat="dd/MM/yyyy"
+      <div className="mb-2">
+        <DateRangeSelector
+          startAndEndDates={getStartAndEndDates()}
+          setStartAndEndDates={setStartAndEndDates}
         />
-        <InputGroup.Text
-          style={{ borderRadius: 0, borderLeft: "none", borderRight: "none" }}
-        >
-          to
-        </InputGroup.Text>
-        <DatePicker
-          selected={getEndDate().toJSDate()}
-          onChange={onEndDateSelection}
-          customInput={
-            <FormControl
-              style={{
-                borderRadius: 0
-              }}
-            />
-          }
-          minDate={getStartDate().toJSDate()}
-          dateFormat="dd/MM/yyyy"
-        />
-        <DropdownButton
-          as={InputGroup.Append}
-          variant="outline-secondary"
-          title="Select Range"
-          id="date-range-dropdown"
-        >
-          {dateRanges.map(dateRange => (
-            <Dropdown.Item
-              onClick={onDateRangeSelection(dateRange)}
-              key={dateRange}
-            >
-              {dateRange}
-            </Dropdown.Item>
-          ))}
-        </DropdownButton>
-      </InputGroup>
+      </div>
 
       <Table striped bordered hover size="sm">
         <thead>
