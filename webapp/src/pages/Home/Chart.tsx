a7b033be8344dd0dbba02d726d89e89aa242259f
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-02-25 23:06:35 +1300

    Vertical bar - however devices are now flipping like crazy

--- webapp/src/pages/Home/Chart.tsx
+++ webapp/src/pages/Home/Chart.tsx
@@ -6,6 +6,8 @@ import { Group } from "@vx/group";
 import { AxisBottom, AxisLeft } from "@vx/axis";
 import { scaleBand, scaleTime } from "@vx/scale";
 import { Circle, Line } from "@vx/shape";
+import { localPoint } from "@vx/event";
+import { WithTooltipProvidedProps } from "@vx/tooltip/lib/enhancers/withTooltip";
 import { EntityIdNameMap } from "./Home";
 
 export interface ChartProps {
@@ -20,6 +22,10 @@ export interface ChartProps {
   height: number;
 }
 
+interface TooltipData {
+  date: DateTime;
+}
+
 const margin = {
   top: 0,
   bottom: 40,
@@ -27,7 +33,7 @@ const margin = {
   right: 0
 };
 
-const Chart: React.FC<ChartProps> = ({
+const Chart: React.FC<ChartProps & WithTooltipProvidedProps<TooltipData>> = ({
   entityIds,
   entityDiscoveryTimes,
   entityIdNameMap,
@@ -36,7 +42,10 @@ const Chart: React.FC<ChartProps> = ({
   maxDate,
   minDate,
   width,
-  height
+  height,
+  tooltipLeft,
+  tooltipData,
+  showTooltip
 }) => {
   const xMax = width - margin.left - margin.right;
   const yMax = height - margin.top - margin.bottom;
@@ -91,74 +100,129 @@ const Chart: React.FC<ChartProps> = ({
     }
   };
 
+  const onChartHover = (onChart: boolean) => (
+    event: React.MouseEvent<SVGRectElement, MouseEvent>
+  ) => {
+    const point = localPoint(event);
+    if (point !== null && onChart) {
+      const mousePosition = point.x - margin.left;
+      const x = xScale.invert(mousePosition);
+      showTooltip({
+        tooltipData: { date: DateTime.fromJSDate(x) },
+        tooltipLeft: mousePosition
+      });
+    } else {
+      showTooltip({ tooltipData: undefined });
+    }
+  };
+
   return (
-    <svg width={width} height={height} className="home-chart">
-      <Group top={margin.top} left={margin.left}>
-        <Group>
-          {entityIds.map(deviceId => {
-            const x1 = xScale(minDate);
-            const x2 = xScale(maxDate);
-            const y = (yScale(deviceId) ?? 0) + yScale.bandwidth() / 2;
-            return (
-              <Line
-                x1={x1}
-                x2={x2}
-                y1={y}
-                y2={y}
-                key={`line-${deviceId}`}
-                className="horizontal-line"
-              />
-            );
-          })}
-        </Group>
-        <Group>
-          {points.map((point, i) => {
-            const x = point[1].toJSDate();
-            const y = point[0];
-            const cx = xScale(x);
-            const cy = (yScale(y) ?? 0) + yScale.bandwidth() / 2;
-            return (
-              <Circle
-                key={`point-${y}-${x.valueOf()}-${i}`}
-                className="dot"
-                cx={cx}
-                cy={cy}
-                r={4}
-              />
-            );
-          })}
-        </Group>
-        <AxisLeft
-          hideAxisLine={true}
-          hideTicks={true}
-          scale={yScale}
-          tickFormat={(value: string, tickIndex: number) =>
-            entityIdNameMap[value]
-          }
-          tickComponent={({ x, y, formattedValue }) => (
-            <text x={x} y={y} textAnchor="end" dy={3} className="entity-label">
-              <tspan onClick={onDeviceNameClick(formattedValue)}>
-                {formattedValue}
-              </tspan>
-              <tspan onClick={onDeviceLinkClick(formattedValue)}> 🔗</tspan>
-            </text>
+    <>
+      <svg width={width} height={height} className="home-chart">
+        <Group top={margin.top} left={margin.left}>
+          <rect
+            x={0}
+            y={margin.top}
+            height={yMax}
+            width={xMax}
+            className="hover-detector"
+            onMouseMove={onChartHover(true)}
+            // onMouseLeave={onChartHover(false)}
+          />
+          <Group
+            onMouseMove={onChartHover(true)}
+            // onMouseLeave={onChartHover(false)}
+          >
+            {entityIds.map(deviceId => {
+              const x1 = xScale(minDate);
+              const x2 = xScale(maxDate);
+              const y = (yScale(deviceId) ?? 0) + yScale.bandwidth() / 2;
+              return (
+                <Line
+                  x1={x1}
+                  x2={x2}
+                  y1={y}
+                  y2={y}
+                  key={`line-${deviceId}`}
+                  className="horizontal-line"
+                />
+              );
+            })}
+          </Group>
+          <Group
+            onMouseMove={onChartHover(true)}
+            // onMouseLeave={onChartHover(false)}
+          >
+            {points.map((point, i) => {
+              const x = point[1].toJSDate();
+              const y = point[0];
+              const cx = xScale(x);
+              const cy = (yScale(y) ?? 0) + yScale.bandwidth() / 2;
+              return (
+                <Circle
+                  key={`point-${y}-${x.valueOf()}-${i}`}
+                  className="dot"
+                  cx={cx}
+                  cy={cy}
+                  r={4}
+                />
+              );
+            })}
+          </Group>
+          {tooltipData && (
+            <rect x={tooltipLeft} y={margin.top} height={yMax} width={2} />
           )}
-        />
-        <AxisBottom
-          top={yMax}
-          scale={xScale}
-          numTicks={10}
-          tickFormat={(value: Date, tickIndex: number) =>
-            DateTime.fromJSDate(value).toFormat("d MMM")
-          }
-          tickLabelProps={(value, index) => ({
-            className: "date-label",
-            textAnchor: "middle"
-          })}
-        />
-      </Group>
-    </svg>
+          <AxisLeft
+            hideAxisLine={true}
+            hideTicks={true}
+            scale={yScale}
+            tickFormat={(value: string, tickIndex: number) =>
+              entityIdNameMap[value]
+            }
+            tickComponent={({ x, y, formattedValue }) => (
+              <text
+                x={x}
+                y={y}
+                textAnchor="end"
+                dy={3}
+                className="entity-label"
+              >
+                <tspan onClick={onDeviceNameClick(formattedValue)}>
+                  {formattedValue}
+                </tspan>
+                <tspan onClick={onDeviceLinkClick(formattedValue)}> 🔗</tspan>
+              </text>
+            )}
+          />
+          <AxisBottom
+            top={yMax}
+            scale={xScale}
+            numTicks={10}
+            tickFormat={(value: Date, tickIndex: number) =>
+              DateTime.fromJSDate(value).toFormat("d MMM")
+            }
+            tickLabelProps={(value, index) => ({
+              className: "date-label",
+              textAnchor: "middle"
+            })}
+          />
+        </Group>
+      </svg>
+      {tooltipData && (
+        <div>
+          <Tooltip
+            top={yMax - 14}
+            left={tooltipLeft}
+            style={{
+              transform: "translateX(50%)"
+            }}
+          >
+            {tooltipData.date.toFormat("f")}
+          </Tooltip>
+        </div>
+      )}
+    </>
   );
 };
 
-export default withTooltip(Chart);
+export default withTooltip<ChartProps, TooltipData>(Chart);
