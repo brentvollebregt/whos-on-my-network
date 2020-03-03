b37494fda63fa0f6e89c7f506e6d607e9093747a
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-03-03 22:05:44 +1300

    Add release workflow

---
+++ run.py
@@ -0,0 +1,9 @@
+"""
+This script is provided as an easy entry point to the whos_on_my_network module
+It is also used to help create binary releases of whos_on_my_network
+"""
+from whos_on_my_network.__main__ import cli
+
+
+if __name__ == '__main__':
+    cli()
